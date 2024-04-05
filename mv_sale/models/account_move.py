4ea476b57512bfd39f4b22528f097de5476ddd70
haitnn <haitnn@eplatform.vn>
2024-04-05 10:31:59 +0700

    .

--- mv_sale/models/account_move.py
+++ mv_sale/models/account_move.py
@@ -7,14 +7,13 @@ from datetime import datetime
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        res = super(AccountMove, self).create(vals_list)
-        for record in res:
-            sale_id = self.env['sale.order'].search([('name', '=', record.invoice_origin)])
-            if len(sale_id) > 0:
-                if not sale_id.date_invoice:
-                    sale_id.write({
-                        'date_invoice': record.create_date,
-                    })
-        return res
+    def action_post(self):
+        for record in self:
+            if record.invoice_origin not in ('', False):
+                sale_id = self.env['sale.order'].search([('name', '=', record.invoice_origin)])
+                if len(sale_id) > 0:
+                    if not sale_id.date_invoice:
+                        sale_id.write({
+                            'date_invoice': datetime.now()
+                        })
+        return super().action_post()
