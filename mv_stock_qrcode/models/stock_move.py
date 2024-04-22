6151bc543f52144e72519270e0feda1eaf0f33ec
Phat Dang (MoveoPlus) <phat.dangminh@moveoplus.com>
2024-04-22 09:15:36 +0700

    [DEV] mv_stock_qrcode: re-check views

--- mv_stock_qrcode/models/stock_move.py
+++ mv_stock_qrcode/models/stock_move.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError, UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 class StockMove(models.Model):
