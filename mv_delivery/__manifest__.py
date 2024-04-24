afe8dcf9d39e2d795cd21462a36ab699c4a54a25
Phat Dang (MoveoPlus) <phat.dangminh@moveoplus.com>
2024-04-24 17:46:43 +0700

    [DEV] mv_delivery: Init new addon to handle on Sale Order for Product Volume

---
+++ mv_delivery/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "MV Delivery",
+    "version": "17.0.1.0",
+    "category": "Moveoplus/MV Delivery",
+    "description": "Base on Delivery module to customize new features",
+    "author": "Phat Dang <phat.dangminh@moveoplus.com>",
+    "depends": [
+        # Odoo
+        "delivery",
+    ],
+    "data": [
+        # VIEWS
+        "views/sale_order_views.xml",
+    ],
+    "license": "LGPL-3",
+    "application": True,
+}
