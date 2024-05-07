e1701131bc7dbbad25146ef62e43af322edf10a5
Phat Dang (MoveoPlus) <phat.dangminh@moveoplus.com>
2024-05-07 17:41:07 +0700

    [DEV] mv_website_sale: init new addon mv_website_sale for remove Brand Promotion

---
+++ mv_website_sale/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "MV eCommerce",
+    "version": "17.0.1.0",
+    "category": "Moveoplus/MV eCommerce",
+    "description": "Base on eCommerce module to customize new features",
+    "author": "Phat Dang <phat.dangminh@moveoplus.com>",
+    "depends": [
+        # Odoo
+        "website_sale",
+    ],
+    "data": [
+        # VIEWS
+        "views/templates.xml",
+    ],
+    "license": "LGPL-3",
+    "application": True,
+}
