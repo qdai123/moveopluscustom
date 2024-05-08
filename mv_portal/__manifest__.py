28b809289b108d95e3043bf945ff09fa9e524227
Phat Dang (MoveoPlus) <phat.dangminh@moveoplus.com>
2024-05-08 17:23:02 +0700

    [DEV] mv_portal: Init new addon

---
+++ mv_portal/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "MV Customer Portal",
+    "version": "17.0.1.0",
+    "category": "Moveoplus/MV Customer Portal",
+    "description": "Base on Customer Portal module to customize new features",
+    "author": "Phat Dang <phat.dangminh@moveoplus.com>",
+    "depends": [
+        # Odoo
+        "portal",
+    ],
+    "data": [
+        # VIEWS
+        "views/portal_templates.xml",
+    ],
+    "license": "LGPL-3",
+    "application": True,
+}
