13445abc0c577cc73958962b064f6434947752cd
Phat Dang (MoveoPlus) <phat.dangminh@moveoplus.com>
2024-05-09 13:59:52 +0700

    [DEV] mv_base,mv_sale: Init new addon Base to Configuration for MOVEOPLUS, update new action workflow Approve for Month in Compute Discount

---
+++ mv_base/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "MV Base",
+    "version": "17.0.1.0",
+    "category": "Moveoplus/MV Base",
+    "description": """
+    The kernel of MOVEO PLUS, needed for all installation.
+    ===================================================
+    """,
+    "author": "Phat Dang <phat.dangminh@moveoplus.com>",
+    "depends": ["base"],
+    "data": [
+        # SECURITY
+        "security/mv_base_groups.xml",
+        "security/mv_base_security.xml",
+        "security/ir.model.access.csv",
+    ],
+    "license": "LGPL-3",
+    "application": True,
+    "auto_install": True,
+}
--- mv_sale/__manifest__.py
+++ mv_sale/__manifest__.py
@@ -1,39 +1,47 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'MV Sale',
-    'description': """
+    "name": "MV Sale",
+    "description": """
         - Discount Agency
     """,
-    'category': '',
-    'version': '17.0',
-    'author': '',
-    'depends': [
+    "category": "",
+    "version": "17.0",
+    "author": "",
+    "depends": [
         # Odoo
-        'website_sale_loyalty',
-        'account',
-        'sale',
-        'sale_stock',
+        "website_sale_loyalty",
+        "account",
+        "sale",
+        "sale_stock",
         # Biz
-        'biz_viettel_sinvoice_v2',
+        "biz_viettel_sinvoice_v2",
+        # Moveoplus
+        "mv_base",
     ],
-
-    'data': [
-        'data/ir_config_parameter_view.xml',
-        'security/ir_rules.xml',
-        'security/ir.model.access.csv',
-        'views/mv_discount_views.xml',
-        'views/res_partner_views.xml',
-        'views/sale_order_views.xml',
-        'views/mv_compute_discount_view.xml',
-        'views/mv_compute_discount_line_views.xml',
-        'views/templates.xml',
-        'report/discount_report_views.xml',
-        'wizard/mv_wizard_discount_views.xml',
-        'wizard/mv_report_discount_views.xml',
+    "data": [
+        # DATA
+        "data/ir_config_parameter_view.xml",
+        # SECURITY
+        "security/res_groups.xml",
+        "security/ir_rules.xml",
+        "security/ir.model.access.csv",
+        # VIEWS
+        "views/mv_discount_views.xml",
+        "views/res_partner_views.xml",
+        "views/sale_order_views.xml",
+        "views/mv_compute_discount_view.xml",
+        "views/mv_compute_discount_line_views.xml",
+        "views/templates.xml",
+        # REPORT
+        "report/discount_report_views.xml",
+        # WIZARD
+        "wizard/mv_wizard_discount_views.xml",
+        "wizard/mv_report_discount_views.xml",
     ],
-    'assets': {
-        'web.assets_frontend': [
+    "assets": {
+        "web.assets_frontend": [
             # 'mv_sale/static/src/js/*.js',
         ],
     },
+    "application": True,
 }
