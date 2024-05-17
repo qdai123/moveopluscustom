ebe687a8737e07a424ce02b02fc85e929a0425c5
Phat Dang (MoveoPlus) <phat.dangminh@moveoplus.com>
2024-05-17 15:11:57 +0700

    [HOTFIX] mv_helpdesk,mv_website_helpdesk: hotfix

--- mv_helpdesk/wizard/wizard_import_lot_serial_number.py
+++ mv_helpdesk/wizard/wizard_import_lot_serial_number.py
@@ -7,51 +7,77 @@ class WizardImportLotSerialNumber(models.TransientModel):
     _name = _description = "wizard.import.lot.serial.number"
 
     helpdesk_ticket_id = fields.Many2one(comodel_name="helpdesk.ticket", readonly=True)
-    portal_lot_serial_number = fields.Text(string="Input Lot/Serial Number")
+    portal_lot_serial_number = fields.Text(string="Input Lot/Serial Number or QR-Code")
 
     def action_import(self):
-        env_call = self.env["helpdesk.ticket"].sudo()
-        error_messages = []
+        codes = self.portal_lot_serial_number
+        if codes:
+            codes_data = self._get_codes_format(codes)
+            messages_list = self._get_message_data(codes_data)
+            error_messages = [
+                message[1]
+                for message in messages_list
+                if message[0]
+                in ["is_empty", "code_not_found", "code_already_registered"]
+            ]
 
-        if self.portal_lot_serial_number:
-            serial_numbers_fmt_list = env_call._format_portal_lot_serial_number(self.portal_lot_serial_number)
-            messages_list = env_call._validation_portal_lot_serial_number(serial_numbers_fmt_list)
+            if error_messages:
+                raise ValidationError("\n".join(error_messages))
 
-            error_messages += [message[1] for message in messages_list if
-                               message[0] in ["serial_number_is_empty",
-                                              "serial_number_not_found",
-                                              "serial_number_already_registered"]]
+        ticket = self.helpdesk_ticket_id
+        if ticket.action_scan_lot_serial_number(self.portal_lot_serial_number):
+            ticket_product_moves_env = self.env["mv.helpdesk.ticket.product.moves"]
+            move_line_env = self.env["stock.move.line"]
 
-        if error_messages:
-            raise ValidationError('\n'.join(error_messages))
-        else:
-            ticket = self.helpdesk_ticket_id
-            scanning_pass = ticket.action_scan_lot_serial_number(self.portal_lot_serial_number)
-            if scanning_pass:
-                ticket_product_moves_env = self.env["mv.helpdesk.ticket.product.moves"]
-                move_line_env = self.env["stock.move.line"]
+            formatted_codes = self._get_codes_format(codes)
+            messages_list = self._get_message_data(formatted_codes)
+            stock_move_line_ids = [
+                move_line[0]
+                for move_line in messages_list
+                if isinstance(move_line[0], int)
+            ]
 
-                messages_list = ticket._validation_portal_lot_serial_number(
-                    ticket._format_portal_lot_serial_number(self.portal_lot_serial_number)
-                )
-                stock_move_line_ids = [
-                    move_line[0] for move_line in messages_list if isinstance(move_line[0], int)
+            stock_move_lines = move_line_env.browse(stock_move_line_ids)
+            existing_ticket_move_lines = ticket_product_moves_env.search(
+                [
+                    ("stock_move_line_id", "in", stock_move_line_ids),
+                    ("helpdesk_ticket_id", "=", False),
                 ]
+            )
 
-                for stock_move_line in move_line_env.browse(stock_move_line_ids):
-                    ticket_stock_move_line_exist = ticket_product_moves_env.search([
-                        ("stock_move_line_id", "=", stock_move_line.id),
-                        ("helpdesk_ticket_id", "=", False)
-                    ], limit=1)
+            existing_ticket_move_lines.sudo().unlink()
 
-                    if ticket_stock_move_line_exist:
-                        ticket_stock_move_line_exist.sudo().unlink()
-                        ticket_product_moves_env.sudo().create({
-                            "stock_move_line_id": stock_move_line.id,
-                            "helpdesk_ticket_id": ticket.id
-                        })
-                    else:
-                        ticket_product_moves_env.sudo().create({
-                            "stock_move_line_id": stock_move_line.id,
-                            "helpdesk_ticket_id": ticket.id
-                        })
+            new_entries = [
+                {"stock_move_line_id": move_line.id, "helpdesk_ticket_id": ticket.id}
+                for move_line in stock_move_lines
+            ]
+
+            ticket_product_moves_env.sudo().create(new_entries)
+
+    # =================================
+    # HELPER/PRIVATE Methods
+    # =================================
+
+    def _get_codes_format(self, codes):
+        """
+        Return the formatted list of codes.
+        :return: []
+        """
+        return (
+            self.env["helpdesk.ticket"].sudo().convert_to_list_codes(codes)
+            if codes
+            else []
+        )
+
+    def _get_message_data(self, codes_data):
+        """
+        Return the message list of codes.
+        :return: []
+        """
+        return (
+            self.env["helpdesk.ticket"]
+            .sudo()
+            ._validation_portal_lot_serial_number(codes_data)
+            if codes_data
+            else []
+        )
