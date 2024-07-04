c4a984af11a1bdb27a0e006b73cf749dd1c42147
Phat Dang Minh <phatdangminh.94@gmail.com>
2024-07-04 12:01:26 +0700

    [FIX] mv_zalo: update

--- mv_zalo/models/res_config_settings.py
+++ mv_zalo/models/res_config_settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
@@ -11,3 +11,23 @@ class ResConfigSettings(models.TransientModel):
         domain="[('use_type', 'in', ['account.move'])]",
         config_parameter="mv_zalo.zns_payment_notification_template",
     )
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        ICPSudo = self.env["ir.config_parameter"].sudo()
+        zns_payment_notification_template_id = ICPSudo.get_param(
+            "mv_zalo.zns_payment_notification_template", False
+        )
+        res.update(
+            zns_payment_notification_template_id=zns_payment_notification_template_id
+        )
+        return res
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        ICPSudo = self.env["ir.config_parameter"].sudo()
+        ICPSudo.set_param(
+            "mv_zalo.zns_payment_notification_template",
+            self.zns_payment_notification_template_id,
+        )
