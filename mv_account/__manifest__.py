17714fd9d02e09e418280c41fc788e4322bf21ce
Phat Dang Minh <phatdangminh.94@gmail.com>
2024-07-06 18:07:17 +0700

    [DEV] mv_account: Init Moveoplus Invoicing

---
+++ mv_account/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "MV Invoicing",
+    "version": "17.0.1.0",
+    "category": "Moveoplus/MV Invoicing",
+    "description": "Base on Invoicing modules and Related modules to customize new features",
+    "author": "Phat Dang <phat.dangminh@moveoplus.com>",
+    "depends": ["account"],
+    "data": [
+        # SECURITY
+        "security/ir.model.access.csv",
+        # VIEWS
+        "views/report_invoice.xml",
+    ],
+    "license": "LGPL-3",
+    "application": True,
+}
