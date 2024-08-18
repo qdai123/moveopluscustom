b97327643f97ccdc0b58be098c476a1ae141e900
anhhuynh97 <ahd.dev.backend@gmail.com>
2024-08-18 22:14:03 +0700

    Add hide menu

---
+++ mv_hide_menu/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MV Hide Menu',
+    'version': '17.0.1.0',
+    'category': 'Moveoplus/MV Hide Menu',
+    'images': ["static/description/icon.png"],
+    'description': "",
+    'author': '',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        # SECURITY
+        # MENU
+        # VIEWS
+        'views/res_users.xml',
+    ],
+    'license': 'LGPL-3',
+    'application': True,
+}
