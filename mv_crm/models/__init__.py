81f8eb877c5db31c5a86890fd637fb24ad662697
hieu3152000 <hieu.bui.set18@eiu.edu.vn>
2024-09-19 17:14:09 +0700

    Fix and validate feedback

--- mv_crm/models/__init__.py
+++ mv_crm/models/__init__.py
@@ -1,1 +1,1 @@
-from . import crm_lead;
+from . import crm_lead
--- mv_crm/models/crm_lead.py
+++ mv_crm/models/crm_lead.py
@@ -3,21 +3,19 @@ from odoo import models, fields, api, _
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
-    _description = 'MV CRM Lead'
 
-    company_registry = fields.Char(related='partner_id.company_registry', string='Customer ID')
-    partner_count = fields.Integer(string="Partner Count", compute='_compute_partner_count')
+    company_registry = fields.Char(related='partner_id.company_registry')
+    partner_survey_count = fields.Integer(string="Partner Count", compute='_compute_partner_count')
 
     @api.depends('partner_id')
     def _compute_partner_count(self):
         for lead in self:
-            partner_records = self.env['mv.partner.survey'].search([('partner_id', '=', lead.partner_id.id)])
-            lead.partner_count = len(partner_records)
+            lead.partner_survey_count = self.env['mv.partner.survey'].search_count([('partner_id', '=', lead.partner_id.id)])
 
     def redirect_mv_dms_view(self):
         self.ensure_one()
         return {
-            'name': _('DMS Files'),
+            'name': _("Partner's Surveys"),
             'view_mode': 'tree',
             'res_model': 'mv.partner.survey',
             'type': 'ir.actions.act_window',
